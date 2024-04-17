9a133500ccdb62e1d0e99905b93f5aff25f827ab
Clara Ruiz-Santacruz Jiménez <clara.ruizsantacruz@gmail.com>
2024-04-17 10:35:41 +0200

    changed filter names

--- artGallery/src/components/Catalogue/Catalogue.jsx
+++ artGallery/src/components/Catalogue/Catalogue.jsx
@@ -5,33 +5,36 @@ function Catalogue() {
     const [selectedCategory, setSelectedCategory] = useState('');
     const handleCategoryClick = (category) => {
         if (selectedCategory === category) {
-            setSelectedCategory(''); 
+            setSelectedCategory('');
         } else {
-            setSelectedCategory(category); 
+            setSelectedCategory(category);
         }
     };
 
 
     return (
         <div className="catalogue-main-container">
-            
+
             <div className="catalogue-body">
-               
+
                 <section className='filters-container'>
-                  
+
                     <h2 id="filters-title" className="filters-title">Nuestras obras</h2>
 
                     <div className='categories-buttons-container'>
-                        <button className={`categories-buttons ${selectedCategory === 'gato' ? 'selected' : ''}`} onClick={() => handleCategoryClick('gato')}>gato</button>
-                        <button className={`categories-buttons ${selectedCategory === 'perro' ? 'selected' : ''}`} onClick={() => handleCategoryClick('perro')}>perro</button>
+                        <button className={`categories-buttons ${selectedCategory === 'Expresionismo' ? 'selected' : ''}`} onClick={() => handleCategoryClick('Expresionismo')}>Expresionismo</button>
+                        <button className={`categories-buttons ${selectedCategory === 'Arte abstracto' ? 'selected' : ''}`} onClick={() => handleCategoryClick('Arte abstracto')}>Arte abstracto</button>
+                        <button className={`categories-buttons ${selectedCategory === 'Realismo contemporáneo' ? 'selected' : ''}`} onClick={() => handleCategoryClick('Realismo contemporáneo')}>Realismo contemporáneo</button>
+                        <button className={`categories-buttons ${selectedCategory === 'Arte digital' ? 'selected' : ''}`} onClick={() => handleCategoryClick('Arte digital')}>Arte digital</button>
+                        <button className={`categories-buttons ${selectedCategory === 'Neo-pop' ? 'selected' : ''}`} onClick={() => handleCategoryClick('Neo-pop')}>Neo-pop</button>
                     </div>
 
-                    <hr className="filters-lines" />
+                  
 
                 </section>
 
                 <section>
-                    <CardGallery selectedCategory={selectedCategory}/>
+                    <CardGallery selectedCategory={selectedCategory} />
                 </section>
 
             </div>
