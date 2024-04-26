23d22ebf9fbb1d8dadfd5bc10fe2b63dc4f2760e
Roger German Cecconello <nia.luna12@gmail.com>
2024-04-26 15:35:43 +0200

    added css and modifications to the userspage component

--- artGallery/src/components/UsersPage/UsersPage.jsx
+++ artGallery/src/components/UsersPage/UsersPage.jsx
@@ -12,48 +12,74 @@ const UsersPage = () => {
     Password: ""
   });
 
+  const clearForm = () => {
+  setFormData({
+    ID_Usertype: "",
+    Name: "",
+    Phone: "",
+    Email: "",
+    Password: ""
+  });
+};
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
     const handleSubmitCreate = async (e) => {
-    e.preventDefault();
-    const user = {
-        ID_Usertype: formData.ID_Usertype,
-        Name: formData.Name,
-        Phone: formData.Phone,
-        Email: formData.Email,
-        Password: formData.Password
-      }
-    try {
-       await axios.post(`http://localhost:5000/adminpage/create`, user);
-
-       const response = await axios.get("http://localhost:5000/adminpage");
-       setUsers(response.data);
-    } catch (error) {
-      console.error("Error al enviar la solicitud de eliminación al servidor:", error);
-    }
+  e.preventDefault();
+  const user = {
+    ID_Usertype: formData.ID_Usertype,
+    Name: formData.Name,
+    Phone: formData.Phone,
+    Email: formData.Email,
+    Password: formData.Password
   };
+  try {
+    await axios.post(`http://localhost:5000/adminpage/create`, user);
 
-     const handleSubmitUpdate = async (e) => {
-          e.preventDefault();
-          const user = {
-              ID_User: formData.ID_User,
-              ID_Usertype: formData.ID_Usertype,
-              Name: formData.Name,
-              Phone: formData.Phone,
-              Email: formData.Email,
-              Password: formData.Password
-            }
-          try {
-            await axios.put(`http://localhost:5000/adminpage/update`, user);
+    const response = await axios.get("http://localhost:5000/adminpage");
+    setUsers(response.data);
 
-            const response = await axios.get("http://localhost:5000/adminpage");
-            setUsers(response.data);
-          } catch (error) {
-            console.error("Error al enviar la solicitud de eliminación al servidor:", error);
-          }
-     };
+    clearForm();
+  } catch (error) {
+    console.error("Error al enviar la solicitud de eliminación al servidor:", error);
+  }
+};
+  const getUserType = (id) => {
+  switch(id) {
+    case 1:
+      return "Administrador";
+    case 2:
+      return "Artista";
+    case 3:
+      return "Cliente";
+    default:
+      return "Desconocido";
+  }
+};
+
+const handleSubmitUpdate = async (e) => {
+  e.preventDefault();
+  const user = {
+    ID_User: formData.ID_User,
+    ID_Usertype: formData.ID_Usertype,
+    Name: formData.Name,
+    Phone: formData.Phone,
+    Email: formData.Email,
+    Password: formData.Password
+  };
+  try {
+    await axios.put(`http://localhost:5000/adminpage/update`, user);
+
+    const response = await axios.get("http://localhost:5000/adminpage");
+    setUsers(response.data);
+
+    clearForm();
+  } catch (error) {
+    console.error("Error al enviar la solicitud de eliminación al servidor:", error);
+  }
+};
 
   const handleEdit = (user) => {
     setFormData({
@@ -90,46 +116,13 @@ const UsersPage = () => {
     fetchAPI();
   }, []);
 
+  
+
   return (
     <>
-    <div>
-      <h1>Usuarios registrados</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Usuario tipo</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Teléfono</th>
-            <th>Dato</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td>{user.ID_User}</td>
-              <td>{user.ID_Usertype}</td>
-              <td>{user.Name}</td>
-              <td>{user.Phone}</td>
-              <td>{user.Email}</td>
-              <td>
-                <button onClick={() => handleEdit(user)}>
-                  Editar
-                </button>
-                <button onClick={() => handleDelete(user)}>
-                  Eliminar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </div>
-
+    <div className='adminpage-container'>
       <form className='form-container'>
-      
-      <div>
+        <div className='input-container'>
         <label htmlFor="Name">Name:</label>
         <input
         className='input-name'
@@ -140,8 +133,8 @@ const UsersPage = () => {
           onChange={handleChange}
           required
         />
-      </div>
-       <div>
+       </div>
+       <div className='input-container'>
         <label htmlFor="Email">Correo Electrónico:</label>
         <input
         className='input-email'
@@ -153,7 +146,7 @@ const UsersPage = () => {
           required
         />
        </div>
-         <div>
+         <div className='input-container'>
            <label htmlFor="Email">Teléfono:</label>
             <input
             className='input-phone'
@@ -166,7 +159,7 @@ const UsersPage = () => {
             />
           </div>
 
-         <div>
+         <div className='input-container'>
             <label htmlFor="Password">Contrasena:</label>
             <input
             className='input-pass'
@@ -179,24 +172,59 @@ const UsersPage = () => {
             />
          </div>
        
-          <div>
+          <div className='input-container'>
             <label htmlFor="Name">ID usertype:</label>
-            <select name="ID_Usertype" id="ID_Usertype" value={formData.ID_Usertype} onChange={handleChange}>
+            <select className='selectadmin' name="ID_Usertype" id="ID_Usertype" value={formData.ID_Usertype} onChange={handleChange}>
               <option value={1} >Administrador</option>
               <option value={2} >Cliente</option>
               <option value={3} >Artista</option>
             </select>
           </div>
         
-       <div style={{padding:"20px"}}>
-          <button type="submit" onClick={handleSubmitCreate}>Añadir Usuario</button>
-          <button type="submit" onClick={handleSubmitUpdate}>Editar Usuario</button>
+       <div className='button-form-container'>
+          <button className='button_form' type="submit" onClick={handleSubmitCreate}>Añadir Usuario</button>
+          <button className='button_form' type="submit" onClick={handleSubmitUpdate}>Editar Usuario</button>
        </div>
       </form>
+     
+
+      <section className='tabla_usuarios'>
+        <h1>Usuarios registrados</h1>
+        <table id='table_content'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Tipo de usuario</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Teléfono</th>
+              <th>Acciones</th>
+           </tr>
+          </thead>
+          <tbody>
+           {users.map((user, index) => (
+              <tr key={index}>
+                <td className='user-data'>{user.ID_User}</td>
+                <td className='user-data'>{getUserType(user.ID_Usertype)}</td>
+                <td className='user-data'>{user.Name}</td>
+                <td className='user-data'>{user.Phone}</td>
+                <td className='user-data'>{user.Email}</td>
+                <td>
+                 <button className='botones_admin_btn' onClick={() => handleEdit(user)}>
+                  Editar
+                 </button>
+                 <button className='botones_admin_btn' onClick={() => handleDelete(user)}>
+                  Eliminar
+                 </button>
+                </td>
+              </tr>
+           ))}
+          </tbody>
+       </table>
+      </section>
+    </div>
     </>
   );
 }
 
 export default UsersPage;
-
-
